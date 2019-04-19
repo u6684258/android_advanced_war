e82bc74e39a5d710ac00e6a26636bad1da72d700
Mingyu <u6684258@anu.edu.au>
2019-04-19 18:10:30 +1000

    more skeleton

--- app/src/main/java/com/example/assignapp2019s1/MainGame.java
+++ app/src/main/java/com/example/assignapp2019s1/MainGame.java
@@ -1,9 +1,12 @@
 package com.example.assignapp2019s1;
 
+import java.util.HashMap;
+
 public class MainGame {
     Board current;
     Player player1;
     Player player2;
+    HashMap moves;
     int turnCount;
     int whoseTurn;
     boolean gameStart;
@@ -16,10 +19,13 @@ public class MainGame {
     //allUnitOfCurrentPlayer();
     //showMovementRange();
     //move();
+    //showattackRange();
+    //showpossibleActions();
     //showDamage();
     //attack();
     //wait();
     //capture();
+    //cancelMove();
     //changeMoney();
     //turnCount();
     //checkGameOver();
